b2453c63fc480f963f3b92063e4c5889bf4dfab6
Deli <A.Delali.Doe@gmail.com>
2026-03-31 08:24:32 +0000

    fixed styling

--- app/page.tsx
+++ app/page.tsx
@@ -9,37 +9,56 @@ export default function Home() {
         <link rel="stylesheet" href="../styles/main.css" />
       </Head>
       <div>
-        
         <main>
-          <h1 id="tagline">YOUR NEXT CONCERT EXPERIENCE<br />STARTS HERE...</h1>
+          <h1 id="tagline">
+            YOUR NEXT CONCERT EXPERIENCE
+            <br />
+            STARTS HERE...
+          </h1>
 
           <h2 id="search">Search by:</h2>
+
           <div className="container">
-              <div>
-                  <label htmlFor="location"><h3>Location</h3></label>
-                  <input type="text" name="location" placeholder="Enter City or Zipcode" />
-                  <button className="babyButton"><a href="/events">&gt;</a></button>
-              </div>
-              <div>
-                  <label htmlFor="date"><h3>Date</h3></label>
-                  <input type="date" name="date" />
-                  <button className="babyButton"><a href="/events">&gt;</a></button>
-              </div>
-              <div>
-                  <label htmlFor="artist"><h3>Artist</h3></label>
-                  <input type="text" name="artist" placeholder="Artist" />
-                  <button className="babyButton"><a href="/events">&gt;</a></button>
-              </div>
+            <div>
+              <label htmlFor="location">
+                <h3>Location</h3>
+              </label>
+              <input type="text" name="location" placeholder="Enter City or Zipcode" />
+              <button className="babyButton">
+                <a href="/events">&gt;</a>
+              </button>
+            </div>
+
+            <div>
+              <label htmlFor="date">
+                <h3>Date</h3>
+              </label>
+              <input type="date" name="date" />
+              <button className="babyButton">
+                <a href="/events">&gt;</a>
+              </button>
+            </div>
+
+            <div>
+              <label htmlFor="artist">
+                <h3>Artist</h3>
+              </label>
+              <input type="text" name="artist" placeholder="Artist" />
+              <button className="babyButton">
+                <a href="/events">&gt;</a>
+              </button>
+            </div>
           </div>
-          <BigButton
-            content={"View All Events"}
-            link={"/events"}
-          />
-          <BigButton
-            content={"Unlock Premium Access"}
-            link={"/membership"}
-          />
-          
+
+          <div className="home-buttons">
+            <BigButton content={"View All Events"} link={"/events"} />
+          </div>
+
+          <div className="premium-box-wrapper">
+            <div className="premium-box">
+              <BigButton content={"Unlock Premium Access"} link={"/membership"} />
+            </div>
+          </div>
         </main>
       </div>
     </>
